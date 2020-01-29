60fd6400643cfb00276dc1afd88af23a4584ed29
mayaepps <mepps@lion.lmu.edu>
2020-01-29 15:04:08 -0800

    added adding functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList'
+import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,12 +9,18 @@ export default function App() {
   ]);
 
   function deleteTodo(id) {
-    setTodos(todos => todos.filter(todo => todo.if !== id));
+    setTodos(todos => todos.filter(todo => todo.id !== id));
   }
+
+  function addTodo(todo) {
+    setTodos(todos => [...todos, todo]);
+  }
+
   return (
     <div className="App">
       <h1>Things To Do</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleter = {deleteTodo}/>
+      <TodoEntryForm adder={addTodo} />
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function TodoList({ todos, deleter }) {
   const todoList = todos.length ? (
     todos.map(todo => {
-        return (
-            <div key={todo.id}>
-                <span onClick={() => deleter(todo.id)}>{todo.content}</span>
-            </div>
-        );
+      return (
+        <div key={todo.id}>
+          <span onClick={() => deleter(todo.id)}>{todo.content}</span>
+        </div>
+      );
     })
   ) : (
     <p>No todos left</p>
